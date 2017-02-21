e25087faa18ada4ec59ee2c7c9ecbc53271944d8
Curtis Hammons <curtishammons@gmail.com>
2017-02-21 14:13:04 -0600

    Green Light |

--- softwareprocess/Sample.py
+++ softwareprocess/Sample.py
@@ -65,7 +65,7 @@ class Sample(object):
         return result
     
     def integrate(self, lowBound, highBound, n, f):
-        epsilon = 0.0009
+        epsilon = 0.0008
         simpsonOld = 0.0
         simpsonNew = epsilon
         s = 4
