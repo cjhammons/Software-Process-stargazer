fa0fb8c48d8a1dbbb99befb975d9ffe93c9dbf53
Curtis Hammons <curtishammons@gmail.com>
2017-04-27 19:12:11 -0500

    Green Light |

--- softwareprocess/correct.py
+++ softwareprocess/correct.py
@@ -11,10 +11,46 @@ def correct(values=None):
         values['error'] = dispatch.ERROR_MANDATORY_INFO_MISSING
         return values
 
+    #check lat
+    if('d' not in values['lat']):
+        values['error'] = dispatch.ERROR_INVALID_LAT
+        return values
+
     lat = dispatch.degreeToDecimal(values['lat'])
+    if (lat <= -90 or lat >= 90):
+        values['error'] = dispatch.ERROR_INVALID_LAT
+
+    #check long
+    if ('d' not in values['long']):
+        values['error'] = dispatch.ERROR_INVALID_LONG
+        return values
     long = dispatch.degreeToDecimal(values['long'])
+    if (long <= 0 or long >= 360):
+        values['error'] = dispatch.ERROR_INVALID_LONG
+        return values
+
+    #check altitude
+    if ('d' not in values['altitude']):
+        values['error'] = dispatch.ERROR_INVALID_ALTITUDE
+        return values
     altitude = dispatch.degreeToDecimal(values['altitude'])
+    if (altitude <= 0 or altitude >= 90):
+        values['error'] = dispatch.ERROR_INVALID_ALTITUDE
+        return values
+
+    #check assumedLat
+    if ('d' not in values['assumedLat']):
+        values['error'] = dispatch.ERROR_INVALID_ASSUMEDLAT
+        return values
     assumedLat = dispatch.degreeToDecimal(values['assumedLat'])
+    if (assumedLat <= -90 or assumedLat >= 90):
+        values['error'] = dispatch.ERROR_INVALID_ASSUMEDLAT
+        return values
+
+    #check assumedLong
+    if ('d' not in values['assumedLong']):
+        values['error'] = dispatch.ERROR_INVALID_LAT
+        return values
     assumedLong = dispatch.degreeToDecimal(values['assumedLong'])
 
     #Local hour angle
